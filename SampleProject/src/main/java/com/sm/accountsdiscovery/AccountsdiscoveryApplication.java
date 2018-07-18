1f5d1f71f404f3823debfab97a7e1638dc7cbbe8
akarsh <cskmrk@yahoo.com>
2018-07-18 12:55:40 +0530

    New Project Added

---
+++ SampleProject/src/main/java/com/sm/accountsdiscovery/AccountsdiscoveryApplication.java
@@ -0,0 +1,12 @@
+package com.sm.accountsdiscovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountsdiscoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountsdiscoveryApplication.class, args);
+	}
+}
---
+++ SampleProject/src/test/java/com/sm/accountsdiscovery/AccountsdiscoveryApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sm.accountsdiscovery;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AccountsdiscoveryApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
